ca5e07989ecaf2111a8732db296806244b8cb34a
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-02-22 23:29:04 +0100

    basis structuur afwerking

--- index.php
+++ index.php
@@ -4,19 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Swarm</title>
 </head>
 <body>
     <header>
-
+        <img src="https://fakeimg.pl/50x50/">
+        <span class="myLocation">in <strong>Kapellen</strong></span>
     </header>
     <main>
         <div class="checkIn">
-        <img src="https://fakeimg.pl/50x50/">
+            <img src="https://fakeimg.pl/75x75/" class="profPic">
+            <div class="dataContainer">
+                <span class="name"><strong>Wesley</strong></span><br>
+                <span class="place">Pool Planet</span><br>
+                <span class="address">F.Rooseveltplaats, Antwerpen</span><br>
+            </div>
+            
         </div>
+        <hr>
     </main>
     <footer>
-    
+        <div class="menu" id="hive"></div>
+        <div class="menu" id="list"></div>
+        <div class="menu" id="cal"></div>
+        <div class="menu" id="search"></div>
     </footer>
 </body>
 </html>
